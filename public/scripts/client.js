543727fbbd27fa4771aa505eb9dc24f97788b4bf
dorreb <ashley_ure@hotmail.com>
2023-02-02 12:51:42 -0500

    no changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,35 +6,33 @@
 
 $(document).ready(function() {
 
-  /**
-   * Takes in a string
-   * Prevents XSS by converting the string to a text node
-   * Returns the inner HTML of the created text node
-   */
+  // Takes in a string
+  // Prevents XSS by converting the string to a text node
+  // Returns the inner HTML of the created text node
+
   const escape = function(str) {
-    let div = document.createElement("div");
+    let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
 
-  /**
-  * Takes in a new tweet object
-  * Returns a new html article, with all tweet information organized into correct elements
-  */
+  // Takes in a new tweet object
+  // Returns a new html article, with all tweet information organized into correct elements
+
   const createTweetElement = function(tweetObj) {
     const $tweet = $("<article>").addClass("tweet");
-    const timeSinceTweet = sinceTweet(tweetObj["created_at"]);
+    const daysAgo = sinceTweet(tweetObj["created_at"]);
 
     const innerHTML = `
           <header>
               <img src= ${tweetObj.user.avatars}>
-              <h4>${tweetObj.user.name}</h4>
-              <p>${tweetObj.user.handle}</p>
+              <span>${tweetObj.user.name}</span>
+              <span class="handle">${tweetObj.user.handle}</span>
           </header>
-          <p>${escape(tweetObj.content.text)}</p>
+          <span>${escape(tweetObj.content.text)}</span>
           <footer>
-            <p>${timeSinceTweet}</p>
+            <span>${daysAgo}</span>
             <span class="interactOptions"><i class="fa-solid fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
           </footer>
           `;
@@ -43,22 +41,22 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  /**
-   * Takes an array of tweet objects
-   * Runs each tweet object through our createTweetElement function
-   * Prepends each returned tweet element to the html section with class 'all-tweets'
-   */
+
+  // Takes an array of tweet objects
+  // Runs each tweet object through our createTweetElement function
+  // Prepends each returned tweet element to the html section with class 'all-tweets'
+
   const renderTweets = function(tweetObjArr) {
     for (const tweet of tweetObjArr) {
       const $tweet = createTweetElement(tweet);
-      $('section.all-tweets').append($tweet);
+      $('section.all-tweets').prepend($tweet);
     }
   };
 
-  /**
-  * Makes a GET request to the tweet database
-  * Runs the returned tweet array through our renderTweets function
-  */
+
+  // Makes a GET request to the tweet database
+  // Runs the returned tweet array through our renderTweets function
+
   const loadTweets = function() {
     $.ajax('/tweets/', { method: 'GET' })
       .then(function(allTweets) {
@@ -68,14 +66,14 @@ $(document).ready(function() {
 
   loadTweets();
 
-  /**
-   * Triggered on submission of the form with the class 'new-tweet'
-   * Empties and slides up any existing error messages
-   * Checks the length of the text being submitted and runs error messages if necessary
-   * If no errors from text length, makes POST request with form text to /tweets/
-   * Then makes a GET request to /tweets/
-   * Then resets the form and uses our renderTweets function to add the new tweet to the page 
-   */
+
+  // Triggered on submission of the form with the class 'new-tweet'
+  // Empties and slides up any existing error messages
+  // Checks the length of the text being submitted and runs error messages if necessary
+  // If no errors from text length, makes POST request with form text to /tweets/
+  // Then makes a GET request to /tweets/
+  // Then resets the form and uses our renderTweets function to add the new tweet to the page 
+
   $('.new-tweet form').submit(function(event) {
     event.preventDefault();
     $('.new-tweet p').empty().slideUp();
@@ -83,12 +81,12 @@ $(document).ready(function() {
     const newTweetTextStr = $form.children('textarea').val();
 
     if (!newTweetTextStr) {
-      $('.new-tweet p').append('<b>Error:</b> All tweets must contain at least one character. Your tweet currently does not.');
+      $('.new-tweet p').append('<b>Error:</b> All tweets must contain at least one character.');
       setTimeout(() => {
         $('.new-tweet p').slideDown();
       }, 600);
     } else if (newTweetTextStr.length > 140) {
-      $('.new-tweet p').append("<b>Error:</b> We do not accept tweets longer than 140 characters. Your tweet is currently too long.");
+      $('.new-tweet p').append("<b>Error:</b> We do not accept tweets longer than 140 characters.");
       setTimeout(() => {
         $('.new-tweet p').slideDown();
       }, 600);
@@ -114,7 +112,6 @@ $(document).ready(function() {
   const sinceTweet = function(epochOfTweet) {
     return timeago.format(epochOfTweet, new Date());
   };
-
 
 
 });
